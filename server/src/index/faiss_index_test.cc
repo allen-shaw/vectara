5aecf0710b84770579352b73ec5c5a5855c6f8cb
allen <allenshaw19@gmail.com>
2025-01-02 00:05:10 +0800

    test(index): fix testsuite name

--- server/src/index/faiss_index_test.cc
+++ server/src/index/faiss_index_test.cc
@@ -10,7 +10,7 @@ TEST(TestFaissIndex, NewFaissIndex) {
     EXPECT_TRUE(index != nullptr);
 }
 
-TEST(TestFaissIndx, InsertAndSearchVectors) {
+TEST(TestFaissIndex, InsertAndSearchVectors) {
     int dim = 1;
     faiss::MetricType faiss_metric = faiss::METRIC_L2;
     FaissIndex* index = new FaissIndex(new faiss::IndexIDMap(new faiss::IndexFlat(dim, faiss_metric)));
