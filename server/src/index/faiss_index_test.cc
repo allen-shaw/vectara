3e01d3dcbebeea78a6d516ad72f32ec1538ced9b
allen <allenshaw19@gmail.com>
2025-01-01 23:57:25 +0800

    test(index): test insert and search

--- server/src/index/faiss_index_test.cc
+++ server/src/index/faiss_index_test.cc
@@ -3,9 +3,20 @@
 #include <faiss/IndexFlat.h>
 #include <faiss/IndexIDMap.h>
 
-TEST(TestNewFaissIndex, NewFaissIndex) {
+TEST(TestFaissIndex, NewFaissIndex) {
     int dim = 1;
     faiss::MetricType faiss_metric = faiss::METRIC_L2;
     FaissIndex* index = new FaissIndex(new faiss::IndexIDMap(new faiss::IndexFlat(dim, faiss_metric)));
     EXPECT_TRUE(index != nullptr);
+}
+
+TEST(TestFaissIndx, InsertAndSearchVectors) {
+    int dim = 1;
+    faiss::MetricType faiss_metric = faiss::METRIC_L2;
+    FaissIndex* index = new FaissIndex(new faiss::IndexIDMap(new faiss::IndexFlat(dim, faiss_metric)));
+    std::vector<float> data = {1.0};
+    index->insert_vectors(data, 0);
+
+    std::pair<std::vector<long>, std::vector<float>> result = index->search_vectors(data, 1);
+    EXPECT_EQ(result.first[0], 0);
 }
