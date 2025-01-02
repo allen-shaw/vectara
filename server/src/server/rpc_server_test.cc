286e3ec681a3b03b38367222701c9aa139fd2ef3
allen <allenshaw19@gmail.com>
2025-01-02 23:25:53 +0800

    test(srv): impl insert and search

--- server/src/server/rpc_server_test.cc
+++ server/src/server/rpc_server_test.cc
@@ -35,18 +35,43 @@ void SendRequest() {
   int ret = channel.Init(server_addr.c_str(), 50051, &options);
   EXPECT_EQ(ret, 0);
 
+  brpc::Controller cntl;
+
   vectara::rpc::RpcService_Stub stub(&channel);
   vectara::rpc::InsertRequest request;
   vectara::rpc::InsertReply reply;
-  brpc::Controller cntl;
+
+  request.add_vectors(0.8);
+  request.add_vectors(9.9);
+  request.set_id(2);
+  request.set_index_type(vectara::index::IndexType::IndexType_Flat);
+
   stub.Insert(&cntl, &request, &reply, nullptr);
   EXPECT_TRUE(!cntl.Failed());
 
   if (cntl.Failed()) {
     LOG(WARNING) << cntl.ErrorText();
+  } else {
+    LOG(INFO) << "Insert success";
   }
 
+  cntl.Reset();
   
+  vectara::rpc::SearchRequest search_request;
+  vectara::rpc::SearchReply search_reply;
+  search_request.add_vectors(0.5);
+  search_request.set_k(2);
+  search_request.set_index_type(vectara::index::IndexType::IndexType_Flat);
+
+  stub.Search(&cntl, &search_request, &search_reply, nullptr);
+  EXPECT_TRUE(!cntl.Failed());
+
+  if (cntl.Failed()) {
+    LOG(WARNING) << cntl.ErrorText();
+  } else {
+    LOG(INFO) << "Search success";
+    LOG(INFO) << search_reply.DebugString();
+  }
 }
 
 TEST(TestRpcServer, RpcRequest) {
